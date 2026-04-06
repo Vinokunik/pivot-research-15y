a94a7651bb950226b57fedf000abafba234a146b
Vinokunik <vinonik@gmail.com>
2026-04-06 21:27:00 +0300

    Update 8 weeks hold.py

--- research_time.py
+++ research_time.py
@@ -11,7 +11,7 @@ from google.oauth2.service_account import Credentials
 SHEET_NAME = 'Pivot Vinokunik'
 INVESTMENT = 1000
 TAX = 0.25
-HOLD_PERIOD = 1 
+HOLD_PERIOD = 8 
 PERIOD = "15y"
 
 def get_combinations():
